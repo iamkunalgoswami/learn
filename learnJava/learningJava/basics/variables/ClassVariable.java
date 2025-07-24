6d9984e9e171338f40b53a18ac8b0fb959c6c329
iamkunalgoswami <kunalgoswami99@gmail.com>
2025-07-24 23:12:15 +0530

    Learning Java - learned the Class Variable vs Local Variable and the default values of Class Variable

---
+++ learnJava/learningJava/basics/variables/ClassVariable.java
@@ -0,0 +1,11 @@
+// package learnJava.learningJava.basics.variables;
+public class ClassVariable {
+    //Class Variable
+    static String number;
+    public static void main(String[] args) {
+        //Local Variable
+        String value = null;
+        System.out.println(number);
+        System.out.println(value);
+    }
+}
