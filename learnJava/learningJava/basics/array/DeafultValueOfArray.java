40f98fab82e1ca9f482795b94abfd404011bfffa
iamkunalgoswami <kunalgoswami99@gmail.com>
2025-07-24 23:24:23 +0530

    Learning Java - learned the about the default values of array soon after declared.

---
+++ learnJava/learningJava/basics/array/DeafultValueOfArray.java
@@ -0,0 +1,9 @@
+package learnJava.learningJava.basics.array;
+class DefaultValueOfArray{
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
