b17a3839968df16bb1425d0a5e8b7a1d5def58c1
iamkunalgoswami <kunalgoswami99@gmail.com>
2025-07-25 00:59:32 +0530

    Learning Java - learning Array - learned the about the declaration of multidimensional-array, using, printing the multidimensional-arrays, default values of multidimensional-array soon after declared, length property of multidimensional-array.

---
+++ learnJava/learningJava/basics/array/MultiDimensionalArray.java
@@ -0,0 +1,76 @@
+package learnJava.learningJava.basics.array;
+
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+        /**
+         * Declaring the Array
+         */
+        int[][] array = new int[3][3];
+        int[][] array1, array2;
+        array1 = new int[3][3];
+        array2 = new int[3][3];
+        int array3[][] = new int[3][3];
+
+        /**
+         * Initializing the Array
+         * 
+         * i -> for rows && j -> for column
+         * i,j = 0, because the start index of any array is 0 (zero)
+         * i,j < 3, because I have initialized every array of size (length to be
+         * precise)
+         * as 3
+         * Not initializing the array3, will print the default value of an array
+         */
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                array[i][j] = (int) (Math.random() * 10);
+                array1[i][j] = (int) (Math.random() * 10);
+                array2[i][j] = (int) (Math.random() * 10);
+                // array3[i] = (int) (Math.random() * 10);
+            }
+        }
+        /**
+         * Printing the Array
+         * 
+         * i = 0, because the start index of any array is 0 (zero)
+         * i < array.length, because I have initialized every array of the same size
+         * We will print the default value of an array
+         */
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(array1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(array2[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(array3[i][j] + " ");
+            }
+            System.out.println();
+        }
+        /**
+         * Printing the Array
+         * 
+         * Using Enhanced for Loop
+         */
+        System.out.println("Printing the Array - Using Enhanced for Loop");
+        for (int i[] : array1) {
+            for (int js : i) {
+                System.out.print(js + " ");
+            }
+            System.out.println();
+        }
+    }
+}
